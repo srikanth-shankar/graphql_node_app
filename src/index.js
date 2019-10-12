780932efb9e8f77fd63f7b09e0f732873a581afb
sshankar <sshankar@cpaglobal.com>
2019-10-12 16:14:53 +1100

    initial

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const { GraphQLServer } = require('graphql-yoga');
+
+const typeDefs = `
+    type Query {
+        title: String!
+        price: Float!
+        releaseYear: Int
+        rating: Float
+        inStock: Boolean!,
+        a: String!
+    }
+`
+
+const resolvers = {
+    Query: {
+        title() {
+            return 'Book'
+        },
+        price(){
+            return 12.99
+        },
+        releaseYear(){
+            return 2000
+        },
+        rating(){
+            return null
+        },
+        inStock(){
+            return true
+        },
+        a = () => {
+            return 'aaa';
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs, resolvers
+})
+
+server.start(()=>{
+    console.log('graphQl server started');
+})
